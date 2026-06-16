7cc945d70f28ab40348c2d8703d2340dd39273c1
Dim Chen <dim.chen@imversed.com>
2026-06-17 01:28:49 +0300

    chore: update version patch to 9 in build.gradle.kts

--- lemuroid-app/build.gradle.kts
+++ lemuroid-app/build.gradle.kts
@@ -27,7 +27,7 @@ buildscript {
 android {
     val versionMajor = 1
     val versionMinor = 10
-    val versionPatch = 8
+    val versionPatch = 9
 
     namespace = "com.swordfish.lemuroid"
     buildFeatures.buildConfig = true
