292e90245e072e8538a81dc1738a31bfcc96e1d1
Dim Chen <dim.chen@imversed.com>
2026-07-15 15:41:20 +0300

    chore: update version patch to 13 and disable Roomcord dialogs

--- lemuroid-app/build.gradle.kts
+++ lemuroid-app/build.gradle.kts
@@ -27,7 +27,7 @@ buildscript {
 android {
     val versionMajor = 1
     val versionMinor = 10
-    val versionPatch = 12
+    val versionPatch = 13
 
     namespace = "com.swordfish.lemuroid"
     buildFeatures.buildConfig = true
